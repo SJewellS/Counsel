f07621f261b3422808e82f2b5636587fbb13f98b
Jewell Esmeralda <160259373+SJewellS@users.noreply.github.com>
2024-02-19 00:06:02 +0800

    Add files via upload

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react"; 
+
+
+function Navbar() {
+  return (
+    <div className="Navbar">
+        <div className="leftSide">
+
+        </div>
+        <div className="rightSide"></div>
+    </div>
+  );
+}
+
+export default Navbar;
